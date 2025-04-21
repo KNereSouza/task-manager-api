376d33194c5051b1392b799d96ac16d4e780407e
KNereS <neres.mp3@gmail.com>
2025-04-21 18:49:05 -0300

    feat: add /task patch route to set an new status for a task

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
-import { Router } from "express";
+import { response, Router } from "express";
 
 // Importing the Controller
 import CreateTaskController from "../controllers/CreateTaskController.js";
 import GetAllTasksController from "../controllers/GetAllTasksController.js";
 import DeleteTaskController from "../controllers/DeleteTaskController.js";
+import SetTaskStatusController from "../controllers/SetTaskStatusController.js";
 
 // Create a new router instance
 const router = Router();
@@ -32,6 +33,12 @@ router.post("/task", async (request, response) => {
 // Route to delete an task
 router.delete("/task", async (request, response) => {
   const controller = new DeleteTaskController();
+  await controller.handle(request, response);
+});
+
+// Route to set an status for a task
+router.patch("/task", async (request, response) => {
+  const controller = new SetTaskStatusController();
   await controller.handle(request, response);
 });
 
