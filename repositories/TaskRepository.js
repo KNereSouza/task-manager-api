9edc05a5f195bf49ff057fcf414966b051902cf8
KNereS <neres.mp3@gmail.com>
2025-04-21 18:47:02 -0300

    feat: add setTaskStatus at ./repositories/TaskRepository.js

--- repositories/TaskRepository.js
+++ repositories/TaskRepository.js
@@ -1,5 +1,8 @@
 import { getDatabase } from "../config/database.js";
 
+const db = getDatabase();
+const tasksCollection = db.collection(process.env.DB_COLLECTION);
+
 /**
  * Saves a task from the database
  * @param   { Task } task - the task to save
@@ -8,12 +11,10 @@ import { getDatabase } from "../config/database.js";
  */
 async function saveTask(task) {
   try {
-    const db = getDatabase();
-    const tasksCollection = db.collection(process.env.DB_COLLECTION);
     await tasksCollection.insertOne(task.toJSON());
     console.log("Task saved to database:", task.toJSON());
   } catch (error) {
-    console.error("Error saving task to database:", error);
+    console.error("[at TaskRepository] Error saving task to database:", error);
     throw new Error("Failed to save task to the database.");
   }
 }
@@ -25,13 +26,11 @@ async function saveTask(task) {
  */
 async function getAllTasks() {
   try {
-    const db = getDatabase();
-    const tasksCollection = db.collection(process.env.DB_COLLECTION);
     const data = await tasksCollection.find().toArray();
     return data;
   } catch (error) {
     console.error(
-      "[at Repository] Error at query all registers from the database.",
+      "[at TaskRepository] Error at query all registers from the database.",
       error
     );
     throw new Error("Failed to query all tasks on the database.");
@@ -40,9 +39,6 @@ async function getAllTasks() {
 
 async function deleteTask(taskId) {
   try {
-    const db = getDatabase();
-    const tasksCollection = db.collection(process.env.DB_COLLECTION);
-
     const query = { id: taskId };
     const result = await tasksCollection.deleteOne(query);
 
@@ -57,11 +53,54 @@ async function deleteTask(taskId) {
     }
   } catch (error) {
     console.error(
-      `[TaskRepository] Failed to delete task with id: ${taskId}`,
+      `[at TaskRepository] Failed to delete task with id: ${taskId}`,
       error
     );
     throw new Error(`Failed to delete task with id: ${taskId}.`);
   }
 }
 
-export { saveTask, getAllTasks, deleteTask };
+async function setTaskStatus(id, status) {
+  try {
+    const filter = { id: id };
+    const updateDoc = {
+      $set: {
+        status: status,
+      },
+    };
+
+    const result = await tasksCollection.updateOne(filter, updateDoc);
+
+    if (result.matchedCount === 0) {
+      console.error(`[at TaskRepository] Task with id '${id}' not found.`);
+      throw new Error(`Task with id '${id}' not found.`);
+    }
+
+    if (result.modifiedCount === 1) {
+      console.log(
+        `[at TaskRepository] Successfully updated task status for id: ${id}`
+      );
+      return { id, status };
+    } else {
+      console.warn(
+        `[at TaskRepository] Task status for id '${id}' was not modified.`
+      );
+      return {
+        id,
+        status: "unchanged",
+        message:
+          "Task status was not modified because the new status is the same as the current status.",
+      };
+    }
+  } catch (error) {
+    console.error(
+      `[at TaskRepository] Error setting task status for ID '${id}' with status '${status}`,
+      error
+    );
+    throw new Error(
+      `INTERNAL SERVER ERROR: Failed to update task status with id: ${id}.`
+    );
+  }
+}
+
+export { saveTask, getAllTasks, deleteTask, setTaskStatus };
