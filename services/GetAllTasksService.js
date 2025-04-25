0b06603ae7b91d4b0a5ddf93cb974038b54370e8
Kauã Neres de Souza <professional.neresk@gmail.com>
2025-04-25 01:21:12 -0300

    doc: JSDoc on the GetAllTasksService class

--- services/GetAllTasksService.js
+++ services/GetAllTasksService.js
@@ -1,6 +1,12 @@
 import { getAllTasks } from "../repositories/TaskRepository.js";
 
 export default class GetAllTasksService {
+
+  /**
+   * Retrieves all tasks from the database
+   * @returns { Promise<Array<Object>> } - A promise that resolves to an array of task objects
+   * @throws  { Error }                  - If the retrieval of tasks fails
+   */
   async handle() {
     try {
       const data = await getAllTasks();
