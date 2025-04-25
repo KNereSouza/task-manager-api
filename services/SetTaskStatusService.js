e463c3286f7bdeb9508209bdb299802e61eb3f8b
Kauã Neres de Souza <professional.neresk@gmail.com>
2025-04-25 01:22:40 -0300

    doc: JSDoc on the SetTaskStatusService class

--- services/SetTaskStatusService.js
+++ services/SetTaskStatusService.js
@@ -1,6 +1,14 @@
 import { setTaskStatus } from "../repositories/TaskRepository.js";
 
 export default class SetTaskStatusService {
+  /**
+   * Updates the status of a task in the database
+   * @param   { Object } params         - The parameters for updating the task status
+   * @param   { string } params.id      - The ID of the task to update
+   * @param   { string } params.status  - The new status to set for the task
+   * @returns { Promise<Object> }       - An object containing the updated task ID and status
+   * @throws  { Error }                 - If the task status update fails
+   */
   async handle({ id, status }) {
     try {
       const data = await setTaskStatus(id, status);
