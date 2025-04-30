1a1be13d2b67d3a5b2f691e7c4fbaf8be811755a
KNereSouza <professional.neresk@gmail.com>
2025-04-30 01:08:10 -0300

    feat: create GetTaskByStatusService class

---
+++ services/GetTaskByStatusService.js
@@ -0,0 +1,16 @@
+import { getTasksByStatus } from "../repositories/TaskRepository.js";
+
+export default class GetTaskByStatusService {
+  async handle({ taskStatus }) {
+    try {
+      const data = await getTasksByStatus(taskStatus);
+      return data;
+    } catch (error) {
+      console.error(
+        `[at services layer] Failed to retrieve tasks by status '${taskStatus}':`,
+        error
+      );
+      throw error;
+    }
+  }
+}
