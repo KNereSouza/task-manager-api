f2ac4258c036490e6266a4979b9dc25651712872
KNereSouza <professional.neresk@gmail.com>
2025-04-29 23:36:54 -0300

    feat: create GetTaskByIdService

---
+++ services/GetTaskByIdService.js
@@ -0,0 +1,26 @@
+import { getTaskByID } from "../repositories/TaskRepository.js";
+
+export default class GetTaskByIdService {
+  /**
+   * Handles retrieving a task by its ID
+   * @param   { Object } params        - The parameters for the retrieval
+   * @param   { string } params.taskId - The ID of the task to retrieve
+   * @returns { Promise<Object> }      - The task object if found
+   * @throws  { Error }                - If the task retrieval fails
+   */
+  async handle({ taskId }) {
+    try {
+      const data = await getTaskByID(taskId);
+      return {
+        sucess: true,
+        data,
+      };
+    } catch (error) {
+      console.error(
+        `[at services layer] Failed to retrieve task with id: ${taskId}`,
+        error
+      );
+      throw error;
+    }
+  }
+}
